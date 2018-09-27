fcf592574938720e3bf44ce8a2ffbefb29430fc9
Schermann <stacytech@yahoo.com>
2018-09-26 21:57:18 -0400

    checking sync between computer and github

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8"/>
+    <meta charset="UTF-8">
     <title>Project 2</title>
     <link rel="stylesheet" href="css/p2stylesheet.css" type="text/css"/>
 </head>
